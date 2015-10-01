9d188ca861774e030e904ab3950c23a9778a4462
Giuseppe Carella <cargius@gmail.com>
2015-10-02 00:00:50 +0200

    added new website

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "tatjana.tsernohh@fokus.fraunhofer.de";
+$subject = $_POST['subject'];
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
+ 
